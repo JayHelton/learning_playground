726807300f34783a3c870076eeb40f7d5b3a1eb2
jayhelton <jaydhelton@gmail.com>
2021-08-08 14:11:06 -0500

    meh

--- hackerman/cryptohack/xor4.py
+++ hackerman/cryptohack/xor4.py
@@ -1,14 +1,12 @@
 #! /usr/bin/python3
 from pwn import xor
 
-encrypted_msg = "0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104"
-encrypted_msg = bytes.fromhex(encrypted_msg)
+encrypted_msg = bytes.fromhex(
+    "0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104"
+)
 
 flag_format = b"crypto{"
-
-key = [o1 ^ o2 for (o1, o2) in zip(encrypted_msg, flag_format)] + [ord("y")]
-
-key_len = len(key)
-flag = xor(bytes(key), encrypted_msg)
-print("Flag:")
+key = xor(encrypted_msg[:7], flag_format) + xor(encrypted_msg[-1], "}")
+print(key)
+flag = xor(key, encrypted_msg)
 print(flag)
